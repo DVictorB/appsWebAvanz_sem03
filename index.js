ba06798cbfe3520f4d0e34febcfdf19e4de99e89
DVictorB <david.balboa@tecsup.edu.pe>
2024-03-25 10:36:26 -0500

    add/new usuario

--- index.js
+++ index.js
@@ -2,32 +2,14 @@ const express = require ("express");
 const port = 3500;
 const app = express();
 
-app.get("/", (req, res)=> {
-    res.send("Hola mundo")
-})
+app.use(express.text())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
-app.get("/myfile", (req, res) => {
-    res.sendFile("./img/tecsup.jpg", {
-    root: __dirname
-    })
-})
 
 app.get("/user", (req, res) => {
-    res.json({
-        nombres:"Genaro",
-        apellidos:"Delgado Parker",
-        edad:65,
-        points:[1,2,3],
-        address:{
-            ciudad: "Lima - Perú",
-            distrito: "San Isidro",
-            calle: "Av. Las gardenias 177"
-        }
-    })
-})
-
-app.get("/isAlive", (req, res) => {
-    res.sendStatus(204)
+    consoles.log(req.body)
+    res.send("Nuevo usuario creado")
 })
 
 app.listen(port)
