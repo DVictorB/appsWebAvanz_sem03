4a487b9266e08c906aec1bc91d0d7fe8ac5cd100
DVictorB <david.balboa@tecsup.edu.pe>
2024-03-25 11:39:21 -0500

    add/routes

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-const express = require ("express");
-const port = 3500;
-const app = express();
+const express = require ("express")
+const port = 3500
+const app = express()
 
-app.use(express.text())
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+// app.use(express.text())
+// app.use(express.json())
+// app.use(express.urlencoded({extended:false}))
 
 
-app.get("/user", (req, res) => {
-    console.log(req.body)
-    res.send("Nuevo usuario creado")
+app.get("/hello/:username", (req, res) => {
+    //console.log(req.params)
+    console.log(typeof req.params.username)
+    res.send(`Hello ${req.params.username.toUpperCase()}`)
+})
+
+app.get("/add/:x/:y", (req, res) => {
+    const {x, y} = req.params
+    res.send(`Result: ${ parseInt(x) + parseInt(y)}`)
+})
+
+app.get("/users/:username/photo", (req, res) => {
+    if(req.params.username.toUpperCase() === "VICTOR") {
+        return res.sendFile("./arquitectura.png", {
+            root: __dirname
+        })
+    }
+    res.send("El usuario no tiene acceso...!!!")
+})
+
+app.get("/name/:name/age/:age", (req, res) => {
+    res.send(`El usuario ${req.params.name} tiene ${req.params.age} años`)
 })
 
 app.listen(port)
-console.log("Server on port #{port}")
+console.log(`Server on port ${port}`)
